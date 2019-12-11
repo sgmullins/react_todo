ce58958169ba23ce9e399c4bd352ecd162c13294
Steve Mullins <sgmullins@gmail.com>
2019-12-11 10:29:29 -0700

    Updates

---
+++ precache-manifest.5a342223e06e77897c5253c574a50f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4bc0ac8badce15428c483e9c753cd4",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "3866e052a8ff93005144",
+    "url": "/react_todo/static/css/main.e8f07b6f.chunk.css"
+  },
+  {
+    "revision": "7a72818eb26eff964252",
+    "url": "/react_todo/static/js/2.b3125f51.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/react_todo/static/js/2.b3125f51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3866e052a8ff93005144",
+    "url": "/react_todo/static/js/main.82ae378a.chunk.js"
+  },
+  {
+    "revision": "ce9a47d618483cce3358",
+    "url": "/react_todo/static/js/runtime-main.acb34139.js"
+  }
+]);
